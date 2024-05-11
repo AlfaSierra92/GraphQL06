b2deced3ec44c490542473ef6b94f4e069de73d6
Antonio Solida <solida.antonio@gmail.com>
2024-05-11 17:27:50 +0200

    Implemented Graphql exceptions handling

--- Chapter06/api/src/main/java/se/magnus/api/core/product/ProductService.java
+++ Chapter06/api/src/main/java/se/magnus/api/core/product/ProductService.java
@@ -1,7 +1,5 @@
 package se.magnus.api.core.product;
 
-import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.*;
 
 public interface ProductService {
